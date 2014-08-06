5f6b64585aa68f82f32e34421ebdd3bcba295e27
Markus Müller <markus@grafixmafia.net>
2014-08-06 22:19:37 +0200

    Initial Commit

---
+++ FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MainFrameStatus
+//
+//  Created by markus on 06.08.14.
+//  Copyright (c) 2014 grafixmafia.net. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
