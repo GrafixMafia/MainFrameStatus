08a03d9095794edb073ce5a209a85b932a9a2f3b
Markus Müller <reg@grafixmafia.net>
2015-05-30 22:33:13 +0200

    Swift changes and apple watch testing

---
+++ spacestate WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  spacestate WatchKit Extension
+//
+//  Created by markus on 26.05.15.
+//  Copyright (c) 2015 grafixmafia.net. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
